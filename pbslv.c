f50e851652b4508c1d358f677644a38bae9d6812
xoyed Ashraf <xoyedashraf@gmail.com>
2025-02-10 20:44:19 +0600

    Add files via upload
    square problem solve

---
+++ pbslv.c
@@ -0,0 +1,13 @@
+/*write a program to calculate a
+ area of circle(side is given)*/
+#include <stdio.h>
+// area of square
+int main (){
+float radius;
+printf("enter radius");
+scanf("%2f", &radius);
+
+
+printf("area is: %f",3.14 *radius * radius);
+return 0;
+}
