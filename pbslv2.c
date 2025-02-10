0985fc570d46245e93ed3be8553631809f82d0a1
xoyed Ashraf <xoyedashraf@gmail.com>
2025-02-10 20:45:32 +0600

    Add files via upload
    a program to calculate a
     area of circle(side is given)

---
+++ pbslv2.c
@@ -0,0 +1,11 @@
+//write a program to canculate area of a square
+#include <stdio.h>
+int main(){
+    // area of square
+    int side;
+    printf("enter side");
+    scanf("%d",&side);
+
+    printf("area is :%d", side *side);
+    return 0;
+}
