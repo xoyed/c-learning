6aacfae0f3a4353734c743c2ea13d9f62e977dc4
xoyed Ashraf <xoyedashraf@gmail.com>
2025-02-11 21:36:48 +0600

    Add files via upload
    lowercase

---
+++ lower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int  main (){
+
+char lower;
+printf("enter any lower letter:");
+scanf("%c",&lower); //a=97
+
+printf("the uppercase letter:%c",lower-32);//A65
+return 0;
+
+
+
+}
