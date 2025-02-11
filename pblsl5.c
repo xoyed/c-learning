bcace3964d450b567a18f50366b95373c9a4809c
xoyed Ashraf <xoyedashraf@gmail.com>
2025-02-11 21:32:10 +0600

    Add files via upload
    triangle problem

---
+++ pblsl5.c
@@ -0,0 +1,18 @@
+// write a program that calculate the area of a traingle 
+#include <stdio.h>
+int main(){
+float base,height,area;
+printf("base=");
+scanf("%f",&base);
+
+printf("height=");
+scanf("%f",&height);
+
+area=(float)1/2*base*height;
+
+printf("area=%.2f\n",area);
+return 0;
+
+
+
+}
