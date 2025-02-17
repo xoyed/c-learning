bb1f3cffabd6c0a5e47d482200d820cda0d350fa
xoyed Ashraf <xoyedashraf@gmail.com>
2025-02-17 22:27:02 +0600

    Add files via upload
    whileloop

---
+++ while..c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main (){
+//int n;
+//printf("enter number:");
+//scanf("%d",&n);
+int i=0;
+while(i<=10){
+    printf("allah\n");
+    i++;
+}
+//int i=0;
+//while(i<=n){
+   // printf("%d\t",i );
+    //i++;
+
+//}
+
+//for(int i=1; i<=n; i++){
+    //printf("%d\t",i);
+//}
+return 0;
+
+
+
+}
